d89b1deb58b5e588ff9b02567b30d50c020686de
ORI <92266719+ORI-Muchim@users.noreply.github.com>
2023-04-21 00:39:58 +0900

    Update ASR.py

--- ASR.py
+++ ASR.py
@@ -2,8 +2,9 @@ import os
 import whisper
 
 model = whisper.load_model("large")
+lang_lst = ['ja', 'japanese']
 
-def process_wav_files(speaker_id, wav_folder, transcript_file):
+def process_wav_files(speaker_id, wav_folder, transcript_file, top_folder):
     with open(transcript_file, "w", encoding='utf-8') as f:
         for wav_file in sorted(os.listdir(wav_folder)):
             if wav_file.endswith(".wav"):
@@ -12,7 +13,6 @@ def process_wav_files(speaker_id, wav_folder, transcript_file):
                     result = model.transcribe(file_path)
                 print(f"{wav_folder}/{wav_file}|{speaker_id}|{result['text'].strip()}")
                 f.writelines(f"{wav_folder}/{wav_file}|{speaker_id}|{result['text'].strip()}\n")
-                # Write to the blueac.txt file
                 with open(os.path.join(top_folder, "blueac.txt"), "a", encoding='utf-8') as blueac_file:
                     blueac_file.writelines(f"{wav_folder}/{wav_file}|{speaker_id}|{result['text'].strip()}\n")
 
@@ -25,7 +25,7 @@ def main():
         if os.path.isdir(folder_path):
             wav_folder = os.path.join(folder_path, "wavs")
             transcript_file = os.path.join(folder_path, "transcript.txt")
-            process_wav_files(speaker_id, wav_folder, transcript_file)
+            process_wav_files(speaker_id, wav_folder, transcript_file, top_folder)
             speaker_id += 1
 
 if __name__ == "__main__":
